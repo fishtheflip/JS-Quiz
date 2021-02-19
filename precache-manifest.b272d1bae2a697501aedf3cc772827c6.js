4bc11ee5e8d4306d5be6d19603fb5627633278ff
Andrei <ataflava@gmail.com>
2021-02-19 21:49:04 +0600

    Updates

---
+++ precache-manifest.b272d1bae2a697501aedf3cc772827c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159f4b1f8789bb78c86167cdc95cafc1",
+    "url": "/JS-Quiz/index.html"
+  },
+  {
+    "revision": "bac86fec00daa586caa3",
+    "url": "/JS-Quiz/static/css/main.7b0f168a.chunk.css"
+  },
+  {
+    "revision": "d54b206c785416778bec",
+    "url": "/JS-Quiz/static/js/2.1fe5106b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JS-Quiz/static/js/2.1fe5106b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac86fec00daa586caa3",
+    "url": "/JS-Quiz/static/js/main.44bbdfd8.chunk.js"
+  },
+  {
+    "revision": "03b80448f5a7243559b2",
+    "url": "/JS-Quiz/static/js/runtime-main.d616af16.js"
+  }
+]);
